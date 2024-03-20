1acc89f46d7c269d0d9062ec88fd985de2c97aef
mason.bartholomai1 <mason.bartholomai1@gmail.com>
2024-03-20 05:06:11 +0000

    update.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <NavigationBar v-if="$route.meta.requiresHeader" />
+    <router-view></router-view>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup>
+import { onMounted, ref } from 'vue';
+import router from './router/index.js';
+import NavigationBar from './components/NavigationBar.vue';
+
+const isAuthenticated = ref(false);
+
+onMounted(() => {
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    isAuthenticated.value = !!user;
+    if (user) {
+      if (route.path === '/login') {
+        router.push('/');
+      }
+    } else {
+      if (route.path !== '/login') {
+        router.push('/login');
+      }
+    }
+  });
+});
+</script>
+
+<style>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,32 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import NavigationBar from './components/NavigationBar.vue'
 
-createApp(App).mount('#app')
+const app = createApp(app);
+app.component('NavigationBar', NavigationBar);
+
+// TailwindCSS
+import 'tailwindcss/tailwind.css'
+
+// Vuex
+import { createStore } from 'vuex'
+const store = createStore({
+    // Your actual Vuex store configuration here
+})
+
+// Heroicons
+import '@heroicons/vue/24/outline'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+const vuetify = createVuetify()
+
+// Vue Router
+import router from './router/index.js'
+
+// Axios
+import axios from 'axios'
+
+app.use(store).use(vuetify).use(router).mount('#app')
